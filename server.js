f9fff935240067e87e82bd9404afa36995941c28
Jesse <135049576+jessek-nti@users.noreply.github.com>
2024-07-29 15:22:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ var server = http.createServer(function (request, response) {
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.write('<p><b>Background Color:</b> '+process.env.BACKGROUND_COLOR+'</p>');
   response.write('<p><b>HostName:</b> '+os.hostname()+'</p>');
-  response.write('<p><b>HostName:</b> '+os.hostname()+'</p>');
   
   response.end();
 })
